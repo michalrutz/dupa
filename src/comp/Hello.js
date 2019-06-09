732097358b95b66fa96273f98eb735df94784637
michalrutz <46682195+michalrutz@users.noreply.github.com>
2019-06-09 14:53:51 +0200

    changed hello

--- src/comp/Hello.js
+++ src/comp/Hello.js
@@ -4,7 +4,7 @@ class Hello extends React.Component {
     render(){
         return (
             <div>
-                <h1>Hello</h1>
+                <h1>Hello 2</h1>
                 {this.props.children}
             </div>
         )
